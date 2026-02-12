8bf343e4057a4a6524e926c6d2ffe320e6f5c18b
Trishitha Chowdhury <trishithachowdhury@gmail.com>
2026-02-13 01:50:44 +0530

    nodeapi.js

---
+++ nodeapi.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
+
