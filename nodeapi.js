7d7f6abf1de248e4eaf4981f03a31fd01a99ce4b
Trishitha Chowdhury <trishithachowdhury@gmail.com>
2026-02-13 19:33:23 +0530

    baby

--- nodeapi.js
+++ nodeapi.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.send('Hello World');
+  res.send('I love you kunal');
 });
 
 const PORT = process.env.PORT || 3000;
